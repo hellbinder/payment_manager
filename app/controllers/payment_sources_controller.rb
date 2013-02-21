d13dd7b5e85dc81a09e24ba15d78fa530b85c2ad
Miguel Martorell <miguel_1216@yahoo.com>
2013-02-21 00:16:24 -0500

    Fixed views. Added sum of amount spent.

--- app/controllers/payment_sources_controller.rb
+++ app/controllers/payment_sources_controller.rb
@@ -2,7 +2,8 @@ class PaymentSourcesController < ApplicationController
   # GET /payment_sources
   # GET /payment_sources.json
   def index
-    @payment_sources = PaymentSource.paginate(:page => params[:page], :per_page => 10)
+    @payment_sources = PaymentSource.paginate(:page => params[:page], per_page: 10)
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @payment_sources }
@@ -13,7 +14,7 @@ class PaymentSourcesController < ApplicationController
   # GET /payment_sources/1.json
   def show
     @payment_source = PaymentSource.find(params[:id])
-
+    @transactions = @payment_source.transactions
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @payment_source }
--- app/models/payment_source.rb
+++ app/models/payment_source.rb
@@ -4,4 +4,13 @@ class PaymentSource < ActiveRecord::Base
   has_many :transactions
   validates_presence_of :name, :payment_source_type_id
   validates_associated :payment_source_type
+
+  def total_amount
+    self.transactions.sum(:amount) # assuming amount is the column holding expenses amount
+  end
+
+  def total_by_month()
+    Transaction.sum(:amount, conditions: {created_at:
+                                              (Date.today.at_beginning_of_month..Date.today.at_end_of_month)})
+  end
 end
